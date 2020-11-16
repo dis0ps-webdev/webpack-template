884803615a363e34b22c709b527962af3cc2a527
dis0ps <dis0ps.email@gmail.com>
2020-11-15 23:12:35 -0500

    fixes import name for AppConfig

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 import * as layout from "../css/layout.global.css";
 import * as typography from "../css/typography.global.css";
-import * as config from "../config/AppConfig.js";
+import * as config from "../config/AppConfig";
 
 const global_layout = layout;
 const global_typography = typography;
