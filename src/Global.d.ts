ad2047ef31ef1aeb8d357e5c79b0296d3fc4ea7e
dis0ps <dis0ps.email@gmail.com>
2020-12-23 01:00:24 -0500

    adds better handling of modular css types

--- src/Global.d.ts
+++ src/Global.d.ts
@@ -1,1 +1,9 @@
-declare module "*.css";
+//Thanks to https://codepen.io/codiechanel/post/css-modules-in-typescript
+
+declare module '*.css' {
+  interface IClassNames {
+    [className: string]: string
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
